71c11a89b59ca62a41968c3e5c90df82537b3a81
aguilar <aguilar.dias@serpro.gov.br>
2018-10-05 09:11:22 -0300

    0
    Signed-off-by: aguilar <aguilar.dias@serpro.gov.br>

--- src/main/java/br/anuncio/dao/AnuncioRepository.java
+++ src/main/java/br/anuncio/dao/AnuncioRepository.java
@@ -1,11 +1,13 @@
 package br.anuncio.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.anuncio.entity.Anuncio;
 
 public interface AnuncioRepository extends CrudRepository<Anuncio, Long> {
 
-	// List<Anuncio> findByLastName(String lastName);
+	List<Anuncio> findByNome(String nome);
 
 }
