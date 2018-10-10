a5d22318fd79be7888e161c8c5a950f3f4d8e73d
aguilar <aguilar.dias@serpro.gov.br>
2018-10-10 14:35:22 -0300

    0
    Signed-off-by: aguilar <aguilar.dias@serpro.gov.br>

--- src/main/java/br/PocSpringApplication.java
+++ src/main/java/br/PocSpringApplication.java
@@ -2,10 +2,9 @@ package br;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 @EnableScheduling
 public class PocSpringApplication {
 
